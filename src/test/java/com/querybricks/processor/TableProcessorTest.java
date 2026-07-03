52bfb6297331a11c60f3a4c8fcb18758d6e352fe
safarislava <safarislava@gmail.com>
2026-07-03 18:48:16 +0300

    test: Add resulted test

--- src/test/java/com/querybricks/processor/TableProcessorTest.java
+++ src/test/java/com/querybricks/processor/TableProcessorTest.java
@@ -2,59 +2,88 @@ package com.querybricks.processor;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
  * Tests for TableProcessor.
  */
 class TableProcessorTest {
-
     @Test
-    public void testCompilationSuccess() {
-        MatcherAssert.assertThat(
-            new Compilation(
-                new StringSourceFile(
-                    "com.querybricks.example.UsersTable",
-                    "package com.querybricks.example;\n"
-                    + "import com.querybricks.annotation.Table;\n"
-                    + "import com.querybricks.column.BoundColumn;\n"
-                    + "import com.querybricks.table.FilterableTable;\n"
-                    + "import java.time.Instant;\n\n"
-                    + "@Table\n"
-                    + "public interface UsersTable extends FilterableTable {\n"
-                    + "    BoundColumn<Long> id();\n"
-                    + "    BoundColumn<String> username();\n"
-                    + "    BoundColumn<String> status();\n"
-                    + "    BoundColumn<Instant> createdAt();\n"
-                    + "}\n"
-                ),
-                new TableProcessor()
-            ).result().isSuccess(),
-            Matchers.equalTo(true)
-        );
-    }
-
-    @Test
-    @Disabled("Enable this once code generation is implemented")
     public void testGeneratedClassContent() {
         MatcherAssert.assertThat(
             new Compilation(
                 new StringSourceFile(
                     "com.querybricks.example.UsersTable",
-                    "package com.querybricks.example;\n"
-                    + "import com.querybricks.annotation.Table;\n"
-                    + "import com.querybricks.column.BoundColumn;\n"
-                    + "import com.querybricks.table.FilterableTable;\n"
-                    + "import java.time.Instant;\n\n"
-                    + "@Table\n"
-                    + "public interface UsersTable extends FilterableTable {\n"
-                    + "    BoundColumn<Long> id();\n"
-                    + "}\n"
+                    """
+                        package com.querybricks.example;
+                        import com.querybricks.annotation.Table;
+                        import com.querybricks.column.BoundColumn;
+                        import com.querybricks.table.FilterableTable;
+                        import java.time.Instant;
+                        
+                        @Table
+                        public interface UsersTable extends FilterableTable {
+                            BoundColumn<Long> id();
+                            BoundColumn<String> username();
+                            BoundColumn<String> status();
+                            BoundColumn<Instant> createdAt();
+                        }
+                        """
                 ),
                 new TableProcessor()
             ).result().fileContent("com/querybricks/example/DbUsersTable.java"),
-            Matchers.containsString("public final class DbUsersTable implements UsersTable")
+            Matchers.equalTo(
+                """
+                    package com.querybricks.example;
+                    
+                    import com.querybricks.column.BoundColumn;
+                    import com.querybricks.column.RawColumn;
+                    import com.querybricks.column.TableColumn;
+                    import java.lang.Long;
+                    import java.lang.Override;
+                    import java.lang.String;
+                    import java.time.Instant;
+                    import java.util.List;
+                    
+                    public final class DbUsersTable implements UsersTable {
+                        private final String name;
+                    
+                        public DbUsersTable(String name) {
+                            this.name = name;
+                        }
+                    
+                        @Override
+                        public final BoundColumn<Long> id() {
+                            return new TableColumn<>(this, new RawColumn<>("id"));
+                        }
+                    
+                        @Override
+                        public final BoundColumn<String> username() {
+                            return new TableColumn<>(this, new RawColumn<>("username"));
+                        }
+                    
+                        @Override
+                        public final BoundColumn<String> status() {
+                            return new TableColumn<>(this, new RawColumn<>("status"));
+                        }
+                    
+                        @Override
+                        public final BoundColumn<Instant> createdAt() {
+                            return new TableColumn<>(this, new RawColumn<>("created_at"));
+                        }
+                    
+                        @Override
+                        public List<BoundColumn<?>> columns() {
+                            return List.of(id(), username(), status(), createdAt());
+                        }
+                    
+                        @Override
+                        public String sql() {
+                            return this.name;
+                        }
+                    }
+                    """
+            )
         );
     }
 }
